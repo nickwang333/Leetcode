5b91d528f058a879e48d0a74acd4f6d401c5158f
zw2922 <89097502+zw2922@users.noreply.github.com>
2021-09-30 02:38:59 -0400

    Create LC 516. Longest Palindromic Subsequence.cpp

---
+++ LC 516. Longest Palindromic Subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        string sr = s;
+        reverse(sr.begin(), sr.end());
+        int n = s.size();
+        vector<int> row(n+1,0);
+        vector<vector<int>> dp(n+1, row);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s[i-1]==sr[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+};
