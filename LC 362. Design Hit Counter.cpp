d64c6c21ffe1cdf8a8c7dcf5e9fd2152971b8396
zw2922 <89097502+zw2922@users.noreply.github.com>
2022-09-13 12:56:17 -0400

    Create LC 362. Design Hit Counter.cpp

---
+++ LC 362. Design Hit Counter.cpp
@@ -0,0 +1,28 @@
+class HitCounter {
+public:
+    queue<int> q;
+    HitCounter() {
+        
+    }
+    
+    void hit(int timestamp) {
+        while(!q.empty() && q.front() <= timestamp-300){
+            q.pop();
+        }
+        q.push(timestamp);
+    }
+    
+    int getHits(int timestamp) {
+        while(!q.empty() && q.front() <= timestamp-300){
+            q.pop();
+        }
+        return q.size();
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
