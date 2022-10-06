6bb7dd3014ee1ae7d40efde242401941b6123caf
zw2922 <89097502+zw2922@users.noreply.github.com>
2022-10-06 10:12:29 -0400

    Create LC 981. Time Based Key-Value Store.cpp

---
+++ LC 981. Time Based Key-Value Store.cpp
@@ -0,0 +1,27 @@
+class TimeMap {
+    unordered_map<string, map<int,string>> m;
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        m[key][timestamp] = value;
+    }
+    
+    string get(string key, int timestamp) {
+        auto it = m[key].upper_bound(timestamp);
+        if(it != m[key].begin()){
+            it--;
+            return it->second;
+        }
+        return "";
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
