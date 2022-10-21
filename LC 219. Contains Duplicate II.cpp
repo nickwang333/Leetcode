bd4e47c8db5fa548b297fa1f0ff18a88aa90c6d6
zw2922 <89097502+zw2922@users.noreply.github.com>
2022-10-21 10:25:09 -0400

    Create LC 219. Contains Duplicate II.cpp

---
+++ LC 219. Contains Duplicate II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, vector<int>> m;
+        for(int i=0; i<nums.size(); i++){
+            m[nums[i]].push_back(i);
+        }
+        for(auto i:m){
+            for(int j=1; j<i.second.size(); j++){
+                if(i.second[j] - i.second[j-1] <= k){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
