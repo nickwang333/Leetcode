5dbdc44369522a43cb7d9ee8f647666abbc27def
zw2922 <89097502+zw2922@users.noreply.github.com>
2023-11-20 15:36:28 -0800

    Create LC 2932. Maximum Strong Pair XOR I.cpp

---
+++ LC 2932. Maximum Strong Pair XOR I.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumStrongPairXor(vector<int>& nums) {
+        int n = nums.size(), ret = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(abs(nums[i] - nums[j]) <= min(nums[i], nums[j])){
+                    ret = max(ret, nums[i] ^ nums[j]);
+                }
+            }
+        }
+        return ret;
+    }
+};
