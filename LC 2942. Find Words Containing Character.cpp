efdffd1c0f88054afd803ae6259bcc05c2a564c9
nickwang333 <89097502+nickwang333@users.noreply.github.com>
2023-11-28 11:50:03 -0800

    Create LC 2942. Find Words Containing Character.cpp

---
+++ LC 2942. Find Words Containing Character.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        int n = words.size();
+        vector<int> ret;
+        for(int i=0; i<n; i++){
+            for(char j:words[i]){
+                if(j == x){
+                    ret.push_back(i);
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+};
