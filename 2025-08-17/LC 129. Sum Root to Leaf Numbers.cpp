ea6558b521d35094e204e9afa7b689259b842884
nickwang333 <89097502+nickwang333@users.noreply.github.com>
2025-08-18 00:00:34 -0700

    Create LC 129. Sum Root to Leaf Numbers.cpp

---
+++ 2025-08-17/LC 129. Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        queue<TreeNode*> q;
+        queue<int> q2;
+        q.push(root);
+        q2.push(root->val);
+        int ret = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i=0; i<sz; i++){
+                TreeNode* tmp = q.front();
+                q.pop();
+                int tmp2 = q2.front();
+                q2.pop();
+                if(tmp->left){
+                    q.push(tmp->left);
+                    q2.push(tmp2*10 + tmp->left->val);
+                }
+                if(tmp->right){
+                    q.push(tmp->right);
+                    q2.push(tmp2*10 + tmp->right->val);
+                }
+                if(!tmp->left && !tmp->right){
+                    ret += tmp2;
+                }
+            }
+        }
+        return ret;
+    }
+};
