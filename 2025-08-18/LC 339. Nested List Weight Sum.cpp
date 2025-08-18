5d8831986955e19fd625831a16365d904a5c399a
nickwang333 <89097502+nickwang333@users.noreply.github.com>
2025-08-18 14:54:42 -0700

    Create LC 339. Nested List Weight Sum.cpp

---
+++ 2025-08-18/LC 339. Nested List Weight Sum.cpp
@@ -0,0 +1,57 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    int depthSum(vector<NestedInteger>& nestedList) {
+        int ret = 0;
+        for(auto i:nestedList){
+            queue<NestedInteger> q;
+            q.push(i);
+            int level = 1;
+            while(!q.empty()){
+                int n = q.size();
+                for(int i=0; i<n; i++){
+                    NestedInteger tmp = q.front();
+                    q.pop();
+                    if(tmp.isInteger()){
+                        ret += tmp.getInteger()*level;
+                    }
+                    else{
+                        for(auto it:tmp.getList()){
+                            q.push(it);
+                        }
+                    }
+                }
+                level++;
+            }
+        }
+        return ret;
+    }
+};
