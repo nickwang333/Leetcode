e284cea4b8bbe0046dfd578880d19840ebc828f4
nickwang333 <89097502+nickwang333@users.noreply.github.com>
2025-08-28 12:53:13 -0700

    Create LC 3. Longest Substring Without Repeating Characters.cpp

---
+++ 2025-08-28/LC 3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size(), p0 = 0, ret = 0;
+        unordered_map<char, int> v;
+        for(int i=0; i<n; i++){
+            v[s[i]]++;
+            while(p0 < i && v[s[i]] > 1){
+                v[s[p0]]--;
+                p0++;
+            }
+            ret = max(ret, i-p0+1);
+        }
+        return ret;
+    }
+};
